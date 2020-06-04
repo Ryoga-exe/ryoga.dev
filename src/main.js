aa1dce1c427e91b2a83916e4a6c660574fe0ac39
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-06-04 23:19:00 +0900

    First commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("theme-switch-btn").onclick = function() {
+        document.body.classList.toggle("darkmode-body");
+    };
+})
