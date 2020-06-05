81afa76c4e8f5f064fb47bc7c0c0d1215804eea9
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-06-05 14:55:24 +0900

    Improve UI and UX

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
+let hogehoge;
+let prev;
 window.addEventListener("DOMContentLoaded", function() {
+    /*
     document.getElementById("theme-switch-btn").onclick = function() {
         document.body.classList.toggle("darkmode-body");
     };
+    */
+    hogehoge = document.getElementById("head");
+    prev = document.documentElement.scrollTop > document.getElementById("cover").clientHeight;
 })
 
 window.onscroll = function() {
     if (document.documentElement.scrollTop > document.getElementById("cover").clientHeight){
         this.console.log("Yes!");
+        if (!prev){
+            hogehoge.style.opacity="1.0"; 
+        }
+        prev = true;
+    }
+    else {
+        if (prev){
+            hogehoge.style.opacity="0";
+        }
+        prev = false;
     }
 }
