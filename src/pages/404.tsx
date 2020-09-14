0253c672f09c46bacabf3729a619ccf81046dc5c
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-09-15 02:58:50 +0900

    Update: typescript化

---
+++ src/pages/404.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import { PageProps, Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+type DataProps = {
+  site: {
+    siteMetadata: {
+      title: string
+    }
+  }
+}
+
+const NotFoundPage: React.FC<PageProps<DataProps>> = ({ data, location }) => (
+  
+  <Layout location={location} title={data.site.siteMetadata.title}>
+    <SEO title="404: Not Found" />
+    <h1>Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
