ba2310343ecf42c16dd0c60fc9b56b73ed0473a7
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-09-18 01:06:30 +0900

    Update: Change date format

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,7 +74,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "YYYY/MM/DD")
             title
             description
           }
