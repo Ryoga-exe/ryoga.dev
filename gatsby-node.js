a725d4231d713adc4bf9d5d180ed5bf39c36efd1
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-09-18 00:49:07 +0900

    Fix: draftを設定しても次の記事に表示される

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,6 +10,7 @@ exports.createPages = async ({ graphql, actions }) => {
       {
         allMarkdownRemark(
           sort: { fields: [frontmatter___date], order: DESC }
+          filter: { frontmatter: { status: { eq: "published" } } }
           limit: 1000
         ) {
           edges {
