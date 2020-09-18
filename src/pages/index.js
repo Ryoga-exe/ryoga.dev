46f73800e376f9e7360bcd21e1f3459229b3066e
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-09-18 23:09:51 +0900

    Update: Change little

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,47 +4,27 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+// import { rhythm } from "../utils/typography"
 
-const BlogIndex = ({ data, location }) => {
+const TopPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="Top" />
       <Bio />
       <Link to="/blog"><h1>Blog</h1></Link>
     </Layout>
   )
 }
 
-export default BlogIndex
+export default TopPage
 
 export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
         title
-      }
-    }
-    allMarkdownRemark(
-        limit: 1000
-        sort: { fields: [frontmatter___date], order: DESC }
-        filter: { frontmatter: { status: { eq: "published" } } }
-      ) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "YYYY/MM/DD")
-            title
-            description
-          }
-        }
       }
     }
   }
