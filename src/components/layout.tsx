e99cbb6b2384d1f8048043e4c57171ecacfb81ab
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-10-04 22:57:41 +0900

    Update: TSX化

---
+++ src/components/layout.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Footer from "./organisms/footer"
+import Dev from "./organisms/development"
+import styled from "styled-components"
+
+const Main = styled.main`
+  margin-left: auto;
+  margin-right: auto;
+  padding-right: 20px;
+  padding-left: 20px;
+  max-width: 1100px;
+  padding-top: 20px;
+  margin-bottom: 20px;
+`
+
+const Layout: React.FC<any> = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header;
+  if (location.pathname === rootPath) {
+    header = (
+      <div></div>
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+  return (
+    <React.Fragment>
+      <Dev />
+      <header>{header}</header>
+      {location.pathname === rootPath ?
+        <main>{children}</main>:
+        <Main>{children}</Main>
+      }
+      <Footer />
+    </React.Fragment>
+  )
+}
+
+export default Layout
