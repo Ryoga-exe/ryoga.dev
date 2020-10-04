a9e8835d708c2648c380189a1255a15e3ea133a9
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-10-04 17:02:32 +0900

    Update prism.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,8 @@
 //import "typeface-montserrat"
 //import "typeface-merriweather"
 import "./src/styles/reset.scss"
-import "./src/styles/global.scss"
 import "prismjs/themes/prism-okaidia.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
+import "./src/styles/prismhighlight.scss"
+
+import "./src/styles/global.scss"
