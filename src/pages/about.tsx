596948ff3bdff2547273935fde2022242ef0b78c
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2021-01-18 04:23:24 +0900

    Update: about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,46 @@ import styled from "styled-components";
 import Bio from "src/components/molecules/bio"
 import Layout from "src/components/layout"
 import SEO from "src/utils/seo"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons'
+
+const H1 = styled.h1`
+  position: relative;
+  font-size: 2.1rem;
+  display: block;
+  > span {
+    margin-left: 2.0em;
+    font-size: 0.45em;
+    opacity: 0.5;
+    > svg {
+      font-size: 0.9em;
+      margin-right: 0.3em;
+    }
+  }
+  @media (max-width: 751px){
+    font-size: 2.0rem;
+    line-height: 1.3;
+    > span{
+      display: block;
+      margin-left: 0;
+    }
+  }
+`
+
+const H2 = styled.h2`
+  position: relative;
+  padding: 0.25em 0;
+  font-size: 1.3rem;
+  margin-top: 1.5rem;
+  margin-bottom: 1.0rem;
+  &::after {
+    content: "";
+    display: block;
+    height: 2px;
+    background: -webkit-linear-gradient(to right, rgb(32, 73, 112), transparent);
+    background: linear-gradient(to right, rgb(32, 73, 112), transparent);
+  }
+`
 
 // ToDo: 見た目の改善
 // ToDo: Skillsの作成 jsonかなんかで管理したい
@@ -18,27 +58,27 @@ const About: React.FC<any> = ({ location }) => {
       <Layout location={location}>
         <SEO title="About" />
         
-        <h1>About</h1>
+        <H1>About<span className="selection-disabled"><FontAwesomeIcon icon={faSyncAlt}/>Last updated: 2021/01/18</span></H1>
 
-        <h2>このサイトについて</h2>
+        <H2>このサイトについて</H2>
         <p>個人サイト兼ブログサイトです。<Link to="/works">自分の作品</Link>もまとめたりします。</p>
         <p>Gatsby.jsで書き、GitHub Pagesでホスティングをしています。ブログはマークダウン形式で書けます！</p>
         <p>一応オープンソースなので、<a href="https://github.com/ryoga-exe/ryoga.dev">GitHubのリポジトリ</a>からソースコード等を見ることができます。</p>
         <p>その他の詳しい技術等はリポジトリのREADMEをご覧ください。</p>
 
-        <h2>Profileなど</h2>
+        <H2>Profileなど</H2>
         <p>Name: Ryoga.exe</p>
         <p>Age: {today<thisYearBirthday?age-1:age}</p>
         <p>Birthday: {birthday.toLocaleDateString()}</p>
         <p>-</p>
         <p>Ryoga.exeというHNで活動しています。茨城県住みです。</p>
-        <p>プログラミングがかなり好きです。基礎はほぼ全ての言語を扱えそう</p>
+        <p>プログラミングがかなり好きです。基礎はほぼ全ての言語を扱えそう。</p>
         <p>競技プログラミングをやってます。主にC++を使います。Web関係の技術も好きです。このサイトをデザインも込みで作れる程度の能力はあります。</p>
         <p>それと、ゲームプログラミングや電子工作も好きです。</p>
 
-        <h2>Skills</h2>
+        <H2>Skills</H2>
 
-        <h2>Links</h2>
+        <H2>Links</H2>
         <a href="https://github.com/ryoga-exe">GitHub</a><br/>
         <a href="https://twitter.com/ryoga_exe">Twitter</a><br/>
         <a href="https://atcoder.jp/users/ryoga_exe">AtCoder</a><br/>
@@ -48,7 +88,7 @@ const About: React.FC<any> = ({ location }) => {
         <a href="https://qiita.com/ryoga-exe">Qiita</a><br/>
         <a href="https://beta.sparebeat.com/users/Ryoga-exe">Sparebeat</a><br/>
 
-        <h2>Experiences</h2>
+        <H2>Experiences</H2>
         <Bio />
       </Layout>
     </React.Fragment>
