8f836da5f051fcf9bda99e41fd56ccc9a72ea562
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2021-02-09 02:58:29 +0900

    Add: dotenv config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
-let activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
 require("dotenv").config({
-  path: `.env.${activeEnv}`,
-});
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   plugins: [
