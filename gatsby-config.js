e79f4b74ebeff2b24b5122230f12e48b743b12d7
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2021-02-09 02:29:59 +0900

    Update: env config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
+let activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${activeEnv}`,
 });
 
 module.exports = {
