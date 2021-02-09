2750e11bda6a70b63f6cad7452378ad05e64f69d
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2021-02-10 01:57:04 +0900

    Update: config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,15 @@ require('dotenv').config()
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-source-qiita`,
+      options: {
+        accessToken: process.env.QIITA_API_KEY,
+        userName: process.env.QIITA_USER_NAME,
+        fetchPrivate: false,
+        // excludedPostIds:
+      }
+    },
+    {
       resolve: `gatsby-plugin-ts-config`,
       options: {
         // specify gatsby-*.ts file's directory
