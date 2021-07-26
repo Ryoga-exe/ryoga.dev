fe8859e6ccf2680d821eda973894ee0ca2b7d456
Ryoga.exe <ryoga.dev@gmail.com>
2021-07-26 23:09:49 +0900

    Update color.ts

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,4 +1,14 @@
+const SocialMedias = {
+  github: '#333',
+  twitter: '#1da1f2',
+  atcoder: '#231815',
+  youtube: '#ff0000',
+  twitch: '#9146ff'
+}
+
 export default {
+  SocialMedias,
+
   bgPrimary: 'white',
   bgSub: 'whitesmoke',
   bgAccent: '#15171a',
