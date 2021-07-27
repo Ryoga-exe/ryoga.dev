880ece75b1a40e63d1cc2dddcf3c07468fa8497a
Ryoga-exe <ryoga.dev@gmail.com>
2021-07-27 17:26:20 +0900

    Update card.tsx

--- src/components/molecules/card.tsx
+++ src/components/molecules/card.tsx
@@ -4,12 +4,16 @@ import Icon from '@components/atoms/icon';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
+import { BreakPoint } from 'src/utils/breakPoint';
+
 const StyledCard = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 25%;
+  padding: 1.0rem;
+  margin-bottom: 1.5rem;
   font-size: 1rem;
-  width: 100%;
 
   > h4 {
     font-weight: 600;
@@ -18,12 +22,24 @@ const StyledCard = styled.div`
   }
 
   > div {
-    font-size: 0.9em;
+    font-size: 1em;
+    white-space: normal;
+    text-align: left;
+  }
+
+  @media (${BreakPoint.CSS.tb}) {
+    width: 33.33333333%;
+  }
+
+  @media (${BreakPoint.CSS.sp}) {
+    width: 50%;
   }
 `;
 
 const StyledFlame = styled.div`
   display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
 
 const Card: React.FC<any> = ({ icon, size, title, children }) => (
