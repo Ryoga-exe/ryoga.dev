f9c24fb3c959d1a294f87049a9f3ec85e2bc5201
Ryoga-exe <ryoga.dev@gmail.com>
2021-07-27 13:23:22 +0900

    Add: prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  importOrderSeparation: true,
+  importOrder: ['^(?!.*/).*$', '^(?!.*\\./).*$', '^(.*\\./).*$'],
+}
