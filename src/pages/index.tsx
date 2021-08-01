973200dd55a2d34b86a1ddd6cb0134272fd021f9
Ryoga.exe <ryoga.dev@gmail.com>
2021-08-01 23:58:04 +0900

    Add plugin: react-tooltip and update top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { PageProps } from 'gatsby';
+import { PageProps, useStaticQuery, graphql } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 import Slider, { Settings } from 'react-slick';
+import ReactTooltip from 'react-tooltip';
 
 import Container from '@components/atoms/container';
 import Heading from '@components/atoms/heading';
@@ -43,33 +44,62 @@ const TopPage: React.FC<PageProps> = ({ location }) => {
 
 export default TopPage;
 
-const Socials = styled.div`
-  font-size: 1.7rem;
-  margin-bottom: 0.7rem;
-  > span {
-    margin-right: 1rem;
-  }
-`;
+const AboutMe: React.FC = () => {
+  const data = useStaticQuery(graphql`
+    query AboutMe {
+      site {
+        siteMetadata {
+          social {
+            twitter
+            github
+          }
+        }
+      }
+    }
+  `);
 
-const AboutMe: React.FC = () => (
-  <section id='aboutMe'>
-    <Container padding='100px 4vw'>
-      <Heading.H3>About Me</Heading.H3>
-      <Socials>
-        <Icon icon={faTwitter} hover='gray' cursor='pointer' />
-        <Icon icon={faGithub} hover='gray' cursor='pointer' />
-      </Socials>
-      <p>茨城県の某中等学校に通ってるただの人間です。</p>
-      <p>ゲーム開発、Webサイト製作、電子工作などいろいろやっています。たまに編み物もやります。</p>
-      <p>最近は、ReactやGatsby.js、競プロにハマって勉強していますが、やりたいことが多すぎて方向性が散乱しがちです。</p>
-      <p>詳しくはAboutページへ</p>
-      <br />
-      <ButtonWrapper>
-        <ButtonDetail to='/about'>About</ButtonDetail>
-      </ButtonWrapper>
-    </Container>
-  </section>
-);
+  const { social } = data.site.siteMetadata;
+
+  const Socials = styled.div`
+    font-size: 1.7rem;
+    margin-bottom: 0.7rem;
+    > a {
+      color: black;
+      text-decoration: none;
+      margin-right: 1rem;
+    }
+  `;
+
+  return (
+    <section id='aboutMe'>
+      <Container padding='100px 4vw'>
+        <Heading.H3>About Me</Heading.H3>
+        <Socials>
+          <a href={`https://twitter.com/${social.twitter}`} target='_blank' rel='noopener noreferrer' data-tip data-for='tooltip-twitter'>
+            <Icon icon={faTwitter} hover='gray' cursor='pointer' />
+          </a>
+          <a href={`https://github.com/${social.github}`} target='_blank' rel='noopener noreferrer' data-tip data-for='tooltip-github'>
+            <Icon icon={faGithub} hover='gray' cursor='pointer' />
+          </a>
+          <ReactTooltip className='tooltip' id='tooltip-twitter' place='top' type='dark' effect='solid'>
+            <span>@{social.twitter}</span>
+          </ReactTooltip>
+          <ReactTooltip className='tooltip' id='tooltip-github' place='top' type='dark' effect='solid'>
+            <span>{social.github}</span>
+          </ReactTooltip>
+        </Socials>
+        <p>茨城県の某中等学校に通ってるただの人間です。</p>
+        <p>ゲーム開発、Webサイト製作、電子工作などいろいろやっています。たまに編み物もやります。</p>
+        <p>最近は、ReactやGatsby.js、競プロにハマって勉強していますが、やりたいことが多すぎて方向性が散乱しがちです。</p>
+        <p>詳しくはAboutページへ</p>
+        <br />
+        <ButtonWrapper>
+          <ButtonDetail to='/about'>About</ButtonDetail>
+        </ButtonWrapper>
+      </Container>
+    </section>
+  );
+};
 
 const Skills: React.FC = () => (
   <section
