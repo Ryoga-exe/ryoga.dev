41da55dec665df3480417697d9970a76f0c15865
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-06-01 15:16:50 +0900

    refactor: delete files

--- src/components/organisms/top/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as AboutMe } from './aboutMe';
-export { default as Skills } from './skills';
-export { default as Works } from './works';
-export { default as RecentPosts } from './recentPosts';
-export { default as Achievements } from './achievements';
