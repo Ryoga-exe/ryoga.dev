19b94410f5193a58271c20e6f24da4ba5dd79bfc
Ryoga-exe <git@ryoga.dev>
2025-07-30 21:03:02 +0900

    chore: update stylelint config

---
+++ stylelint.config.mjs
@@ -0,0 +1,45 @@
+/** @type {import("stylelint").Config} */
+export default {
+  customSyntax: "postcss-html",
+  plugins: [
+    "stylelint-scss",
+    "stylelint-declaration-block-no-ignored-properties",
+    "stylelint-no-unsupported-browser-features",
+    "stylelint-prettier",
+  ],
+  extends: ["stylelint-config-standard-scss", "stylelint-config-recess-order", "stylelint-config-prettier-scss"],
+  ignoreFiles: ["node_modules/**/*"],
+  rules: {
+    "prettier/prettier": true,
+    "plugin/declaration-block-no-ignored-properties": true,
+    "plugin/no-unsupported-browser-features": [
+      true,
+      {
+        severity: "warning",
+        ignore: ["css-nesting"],
+      },
+    ],
+    "font-family-no-missing-generic-family-keyword": true,
+    "declaration-block-no-shorthand-property-overrides": true,
+    "selector-pseudo-element-colon-notation": "double",
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"],
+      },
+    ],
+    "no-duplicate-selectors": [
+      true,
+      {
+        severity: "warning",
+      },
+    ],
+    "property-no-vendor-prefix": [
+      true,
+      {
+        ignoreProperties: ["backdrop-filter"],
+      },
+    ],
+    "selector-class-pattern": null,
+  },
+};
