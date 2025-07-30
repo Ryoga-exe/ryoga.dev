c3ba45029f3a8ffc492166fb3fd4176408b13b5d
Ryoga-exe <git@ryoga.dev>
2025-07-30 21:02:13 +0900

    chore: update prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
